f0e4e1bca95a61dd1c1b4ce256463d5fca03f8b1
teju1025 <s1092921@mail.ncyu.edu.tw>
2022-06-07 15:34:13 +0800

    新增主畫面圖片

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react'
 export default function Home() {
     return (
         <div>
-            
+            <img src={require("../logo512.png")} alt="Background"/>
         </div>
     )
 }
